1a7d3777a504c41caf9a548b70eaeb4625c8d393
Vlad Shturma <uchiha248@mail.ru>
2013-07-17 16:17:31 +0200

    Finished tests for PostsController

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -19,4 +19,25 @@ class PostsControllerTest < ActionController::TestCase
     assert_redirected_to post_path(assigns(:post))
   end
 
+  test "should show all posts" do
+    get :index
+    assert_response :success
+  end
+
+  test "should update post" do
+    authenticate
+    get :update, id: @post.id, post: {title: 'Hello', text: 'This is my first post.'}
+    assert_redirected_to post_path(assigns(:post))
+    assert_equal('Hello',assigns(:post).title)
+    assert_equal('This is my first post.',assigns(:post).text)
+  end
+
+  test "should destroy post" do
+    authenticate
+    assert_difference('Post.count', -1) do
+      delete :destroy, id: @post.id
+    end
+    assert_redirected_to posts_path
+  end
+
 end
