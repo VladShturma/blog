52e774222cd802b9ee10e7b842bc91e1a154ecee
Vlad Shturma <uchiha248@mail.ru>
2013-07-17 22:48:30 +0200

    Tests for CommentsController

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    posts(:one).comments << comments(:one)
+  end
+
+  test "should create comment" do
+    assert_difference('posts(:one).comments.count') do
+      post :create, post_id: posts(:one).id, comment: {commenter: "Vlad Shturma", body: "My comment"}
+    end
+    assert_redirected_to post_path(assigns(:post))
+  end
+
+  test "should delete comment" do
+    authenticate
+    assert_difference('posts(:one).comments.count', -1) do
+      delete :destroy, post_id: posts(:one).id, id: comments(:one).id
+    end
+  end
+
 end
