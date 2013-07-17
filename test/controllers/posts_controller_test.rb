d537b2b4465f3df7f9fba0a84862e03b9c44efa1
Vlad Shturma <uchiha248@mail.ru>
2013-07-17 15:25:27 +0200

    Added tests for post#show and post#create

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @post = posts(:one)
+  end
+
+  test "should show post" do
+    get :show, id: @post.id
+    assert_response :success
+  end
+
+  test "should create post" do
+    authenticate
+    assert_difference('Post.count') do
+      post :create, post: {title: 'Hello', text: 'This is my first post.'}
+    end
+    assert_redirected_to post_path(assigns(:post))
+  end
+
 end
